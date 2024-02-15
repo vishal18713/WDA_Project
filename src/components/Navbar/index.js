0906a1936f636f54ba36a9e91a63317229b58313
vishal18713 <lcs2023052@iiitl.ac.in>
2024-02-15 18:08:31 +0530

    Refactor Navbar component and update styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Logo,Nav, Navcontainer,MobileIcon,NavMenue,NavLinks,Navitem } from './Navbar.js';
-import { FaBarsStaggered as Bars } from "react-icons/fa6";
+import React from "react";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+import "./Navbar.css";
+import  { useState } from 'react';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
-    <Nav>
+      {/* <Nav>
         <Navcontainer>
             <Logo to='/'>vishal</Logo>
             <MobileIcon>
@@ -23,9 +26,47 @@ const Navbar = () => {
                 </Navitem>
             </NavMenue>
         </Navcontainer>
-    </Nav>
-    </>
-  )
-}
+    </Nav> */}
 
-export default Navbar
+      <div className="Navbar">
+        <div className="Navbar_link">
+          <div className="Navbar_link_logo">VISHAL</div>
+          <div className="Navbar_link_container">
+                <p><a>Home</a></p>
+                <p><a>About</a></p>
+                <p><a>contact</a></p>
+                <p><a>visit</a></p>
+                <p><a>call</a></p>
+          </div>
+        </div>
+        <div className="Navbar_sign">
+        
+        <button type="button">Sign up</button>
+        </div>
+        <div className="Navbar_menu">
+        {toggleMenu
+          ? <RiCloseLine color="black" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="black" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="Navbar_menu_container scale-up-center">
+          <div className="Navbar_menu_container_links">
+                <p><a>Home</a></p>
+                <p><a>About</a></p>
+                <p><a>contact</a></p>
+                <p><a>visit</a></p>
+                <p><a>call</a></p>
+          </div>
+          <div className="Navbar_menu_container_links_sign">
+            
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
