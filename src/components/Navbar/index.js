2b584202d0113ae5a05b35a561b21291a83b7c24
vishal18713 <lcs2023052@iiitl.ac.in>
2024-02-16 09:22:48 +0530

    Refactor Navbar component styles and links***

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,50 +1,75 @@
 import React from "react";
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 import "./Navbar.css";
-import  { useState } from 'react';
+import { useState } from "react";
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
-
-
       <div className="Navbar">
         <div className="Navbar_link">
           <div className="Navbar_link_logo">VISHAL</div>
           <div className="Navbar_link_container">
-                <p><a href="/home">Home</a></p>
-                <p><a href="/about">About Us</a></p>
-                <p><a href="/contact">contact</a></p>
-                <p><a href="/visit">visit</a></p>
-                <p><a href="/call">call</a></p>
+            <p>
+              <a>Home</a>
+            </p>
+            <p>
+              <a>About</a>
+            </p>
+            <p>
+              <a>Contact</a>
+            </p>
+            <p>
+              <a>Visit</a>
+            </p>
+            <p>
+              <a>Call</a>
+            </p>
           </div>
         </div>
         <div className="Navbar_sign">
-        
-        <button type="button">Sign up</button>
+          <button type="button">Sign up</button>
         </div>
         <div className="Navbar_menu">
-        {toggleMenu
-          ? <RiCloseLine color="white" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color="white" size={27} onClick={() => setToggleMenu(true)} />}
-        {toggleMenu && (
-        <div className="Navbar_menu_container scale-up-center">
-          <div className="Navbar_menu_container_links">
-                <p><a href="/home">Home</a></p>
-                <p><a href="/about">About Us</a></p>
-                <p><a href="/contact">contact</a></p>
-                <p><a href="/visit">visit</a></p>
-                <p><a href="/call">call</a></p>
-          </div>
-          <div className="Navbar_menu_container_links_sign">
-            
-            <button type="button">Sign up</button>
-          </div>
-        </div>
-        )}
-
+          {toggleMenu ? (
+            <RiCloseLine
+              color="white"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="white"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="Navbar_menu_container scale-up-center">
+              <div className="Navbar_menu_container_links">
+                <p>
+                  <a>Home</a>
+                </p>
+                <p>
+                  <a>About</a>
+                </p>
+                <p>
+                  <a>Contact</a>
+                </p>
+                <p>
+                  <a>Visit</a>
+                </p>
+                <p>
+                  <a>Call</a>
+                </p>
+              </div>
+              <div className="Navbar_menu_container_links_sign">
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
