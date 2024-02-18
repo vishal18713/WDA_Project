bf48f93e9a25076a272c0f1d85a4fefafc0e9ece
vishal18713 <lcs2023052@iiitl.ac.in>
2024-02-18 23:04:31 +0530

    Remove unnecessary code from App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
-      <Router>
+      
       
         <Navbar></Navbar>
-      </Router>
+      
     </>
   );
 }
