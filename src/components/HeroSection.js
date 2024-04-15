77e5d89d01ea73fb09f75ca789285eec4b7ca300
vishal18713 <lcs2023052@iiitl.ac.in>
2024-04-15 16:14:25 +0530

    Refactor package.json to add react-icons and react-router-dom dependencies and crearted footer,navbar and hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+       
+    </div>
+  );
+}
+
+export default HeroSection;
